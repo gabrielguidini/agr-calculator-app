ddcfa1e17cabb4c6b0c42082d06c0e8339d2397c
gabrielguidini <gabrielsguidini@gmail.com>
2023-04-18 19:20:36 -0300

    teste 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //TENTATIVA 01) 
 function inputDay (){
-    let valorInput =document.forms["submit"]["fname"].value;
+    let valorInput =document.forms["submit"].value;
     //let month =document.getElementById(month);
     //let year =document.getElementById(year);
     if(valorInput<0 || valorInput== ""){
