f63d1ed9905662c653cb521e01d66295f5228a66
gabrielguidini <gabrielsguidini@gmail.com>
2023-04-18 19:15:38 -0300

    testing

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+//TENTATIVA 01) 
+function inputDay (){
+    let valorInput =document.forms["submit"]["fname"].value;
+    //let month =document.getElementById(month);
+    //let year =document.getElementById(year);
+    if(valorInput<0 || valorInput== ""){
+        alert("não foi dessa vez");
+    }
+}
