f8995bd9896d295eaa4f741e927dcd16de4f90f1
gabrielguidini <gabrielsguidini@gmail.com>
2023-04-19 15:08:49 -0300

    last commit

--- js/submit.js
+++ js/submit.js
@@ -1,29 +1,45 @@
 //QuerySelector
 
+let validation = true;
+
 function calculate(){
     const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const currentDay = new Date().getDate();
-                
-    let years = currentYear - yearInput.value;
-    let month = currentMonth - monthInput.value * 12;
-    let days = currentDay - dayInput.value;
-                
-    console.log(years,month,days);
-                
-}
-function inputVal(){
-    const dayInput = document.getElementById("day").value;
-    const monthInput = document.getElementById("month").value;
-    const yearInput = document.getElementById("year").value;
+    const yearInput = Number(document.getElementById("year").value);
+    parseInt(yearInput);
+        
+    let yearO = (currentYear) - (yearInput);
+    let monthO =  (yearO * 12);
+    let daysO =  (yearO * 360);
+    console.log(yearO,monthO,daysO);
 
-    if(dayInput<0 || dayInput>32 || monthInput>13 || monthInput<0|| yearInput<1900 || yearInput<0 || isNaN(dayInput)|| isNaN(monthInput)|| isNaN(yearInput)){
-        document.getElementById("day").style.color= "red"
-        document.getElementById("month").style.color= "red"
-        document.getElementById("year").style.color= "red"
-        //error = document.getElementById("error").style.
-        return alert("faio");
-    }
-    calculate();
-}
+    let yearOut = document.getElementById("year-out");
+    let monthOut = document.getElementById("month-out");
+    let dayOut = document.getElementById("day-out");
+
+    yearOut.innerHTML = yearO;
+    monthOut.innerHTML = monthO;
+    dayOut.innerHTML = daysO;
     
+    //document.getElementById("year-out").replaceWith(yearO);
+    //document.getElementById("month-out").replaceWith(monthO);
+    //document.getElementById("day-out").replaceWith(daysO);
+    return validation = false;
+}
+if(validation){
+    function inputVal(){
+        const dayInput = Number(document.getElementById("day").value);
+        const monthInput = Number(document.getElementById("month").value);
+        const yearInput = Number(document.getElementById("year").value);
+    
+        console.log(typeof( dayInput,monthInput,yearInput))
+    
+        if(dayInput<0 || dayInput>32 || monthInput>13 || monthInput<0|| yearInput<1900 || yearInput<0 || isNaN(dayInput)|| isNaN(monthInput)|| isNaN(yearInput)){
+            document.getElementById("day").style.color= "red"
+            document.getElementById("month").style.color= "red"
+            document.getElementById("year").style.color= "red"
+            error = document.getElementById("error").style.color = "red"
+            return alert("faio");
+        }
+        calculate();
+    }
+}
